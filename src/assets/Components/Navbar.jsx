76154e5a9d1eb4ded2f537c8569b2e6e91cec87e
Saurabh7-sys <saurabhshelar512@gmail.com>
2024-08-22 16:46:05 +0530

    Added footer component and made relevant updates

--- src/assets/Components/Navbar.jsx
+++ src/assets/Components/Navbar.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import { Link , useNavigate } from 'react-router-dom';
-import Button from './components/button';
+import Button from './button';
 
 const Navbar = () => {
   const [open, setOpen] = useState(false);
